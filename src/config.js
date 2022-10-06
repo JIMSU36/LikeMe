6924a9f8469ff7a54ba4e4900ab55a4600a2fef6
JIMSU36 <rlawltn993618@gmail.com>
2022-10-06 18:26:31 +0900

    api 주소

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    restApi: "ec2-54-180-152-45.ap-northeast-2.compute.amazonaws.com", // API
+    restApi: "http://ec2-54-180-152-45.ap-northeast-2.compute.amazonaws.com", // API
   };
   
